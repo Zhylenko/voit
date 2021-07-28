27a21653ee4a24db92a365b31c3207a5e4472415
Vladislav Zhylenko <zhylenko.vlad@gmail.com>
2021-07-28 19:58:44 +0300

    Cleared challenge popup.

--- resources/views/home/challenge.blade.php
+++ resources/views/home/challenge.blade.php
@@ -11,24 +11,9 @@
         </div>
 
         <div class="popup__question" id="test-question">
-            Question Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, molestiae fuga optio ducimus, ratione, eaque blanditiis earum voluptates laudantium autem impedit excepturi tenetur magni nam. Minus possimus dolores aut illo?
         </div>
-        <div class="form-error test-error">Необходимо заполнить пустое поле</div>
+        <div class="form-error test-error"></div>
         <div class="popup__group">
-            <div class="form__radio-btn">
-                <input class="popup__group-btn answer" id="radio-1" type="radio" value="1" name="answer-radio">
-                <label for="radio-1" style="background-image: url({{ asset('img/lines.svg') }});">Да</label>
-            </div>
-
-            <div class="form__radio-btn">
-                <input class="popup__group-btn answer" id="radio-2" type="radio" value="2" name="answer-radio">
-                <label for="radio-2" style="background-image: url({{ asset('img/lines.svg') }});">Нет</label>
-            </div>
-
-            <div class="form__radio-btn">
-                <input class="popup__group-btn answer" id="radio-3" type="radio" value="3" name="answer-radio">
-                <label for="radio-3" style="background-image: url({{ asset('img/lines.svg') }});">Больше 3 часов в день.</label>
-            </div>
         </div>
 
         <button class="popup__next" id="next">Далее</button>
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -14,9 +14,11 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- Libraries -->
     <script src="{{ asset('js/libs.min.js') }}"></script>
+    @if(isset($auth) && $auth === true)
     @if($passed !== true)
     <!-- Challenge -->
     <script src="{{ asset('js/challenge.js') }}"></script>
+    @endif
     @endif
     <!-- Main scripts -->
     <script src="{{ asset('js/main.js') }}"></script>
