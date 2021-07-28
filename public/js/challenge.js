141112435dec433e1bf78dcbb3ac3ae44f63fe9b
Max-Tsymba <young.bezero@gmail.com>
2021-07-28 21:25:01 +0300

    generate test js

--- public/js/challenge.js
+++ public/js/challenge.js
@@ -1,13 +1,14 @@
-postTestFormRequests('.overlay__test', '.form__radio-btn', '.answer', 'next', 'popup-Btn', config.endPoints['challenge-get']);
+postTestFormRequests('.overlay__test', '.form__radio-btn', '.answer', 'next', 'popup-Btn', '.popup__group', config.endPoints['challenge-get']);
 removeTest('.overlay__test', '.test-close', '.test-error');
 
-function postTestFormRequests(modalOverlayClass, radioGroupClass, answerGroupClass, submitBtnID, modalBtnID, url) {
+function postTestFormRequests(modalOverlayClass, radioGroupClass, answerGroupClass, submitBtnID, modalBtnID, modalGroupClass ,url) {
         
     const radioBtns = document.querySelectorAll(radioGroupClass),
             answers = document.querySelectorAll(answerGroupClass),
             submitBtn = document.getElementById(submitBtnID),
             modalBtn = document.getElementById(modalBtnID),
-            modal = document.querySelector(modalOverlayClass);
+            modal = document.querySelector(modalOverlayClass),
+            group = document.querySelector(modalGroupClass);
     
     let errorLabel = document.querySelector('.test-error');
     let question = null;
@@ -21,6 +22,7 @@ function postTestFormRequests(modalOverlayClass, radioGroupClass, answerGroupCla
             event.preventDefault();
             
             question = document.getElementById('test-question');
+            group.innerHTML = '';
 
             let input = '';
 
@@ -55,6 +57,11 @@ function postTestFormRequests(modalOverlayClass, radioGroupClass, answerGroupCla
 
                     let result = await response.json();
                     question.textContent = result.question;
+
+                    for(let index = 0; index < result.answers.length; index++) {
+                        group.insertAdjacentHTML('beforeend', generateAnswers(index+1, index+1, result.answers.answer));
+                    }
+                    
                     console.log(result);
 
                     if(isFirst) modal.style.display = 'block';
@@ -85,4 +92,13 @@ function removeTest(modalOverlayClass, closeBtnClass, errorLabelClass) {
             modal.style.display = 'none';
         });
     }
+}
+
+function generateAnswers(index, value, text) {
+    return `
+    <div class="form__radio-btn">  
+        <input class="popup__group-btn answer" id="radio-${index}" type="radio" value="${value}" name="answer-radio">
+        <label for="radio-${index}" style="background-image: url(./img/lines.svg);">${text}</label>
+    </div>
+    `;
 }
