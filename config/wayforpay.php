565703f6b72a8e9bd66289b9bd55153ed2889130
Vladislav Zhylenko <zhylenko.vlad@gmail.com>
2021-08-18 14:40:29 +0300

    HTML lang

---
+++ config/wayforpay.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password timout (in seconds)
+    |--------------------------------------------------------------------------
+    |
+    | Timeout to send new password to register
+    |
+    */
+    'merchant' => [
+        'login' => 'voitivitlife_com',
+        'secret_key' => '8d6568caa41f3825d6ea7fd1cef4bd16c8617e83',
+    ],
+];
